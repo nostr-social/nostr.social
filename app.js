63b4a6a05703c5135b00dcc989d91c3c23958fe8
Melvin Carvalho <melvincarvalho@gmail.com>
2023-05-14 16:54:22 +0200

    add github icon

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ function doc() {
 
 class UserProfile extends Component {
   render() {
-    const { userPublicKey, name, picture, about, banner } = this.props
+    const { userPublicKey, name, picture, about, banner, github } = this.props
     const key = getQueryStringValue('pubkey') || userPublicKey
     const irisLink = `https://iris.to/${key}`
     const canonical = `/.well-known/nostr/pubkey/${key}/index.json`
+    const githubLink = github
     const shortenedPubKey = key ? key.slice(0, 16) : ''
 
     return html`
@@ -31,16 +32,20 @@ class UserProfile extends Component {
         ${banner ? html`<div class="banner"><img src="${banner}" alt="Banner" /></div>` : ''}
         <div class="profile-details">
           <img src="${picture}" alt="Profile Picture" class="user-picture" />
-          <a style="text-decoration:none" href="${canonical}" target="_blank" class="profile-link">🔗</a>
           <h2><span title="In the Nostr Strong Set">🛡️</span> ${name}</h2>
           
           ${key ? html`<p class="pubkey">Pubkey: <a href="${irisLink}" target="_blank">${shortenedPubKey}</a></p>` : ''}
           ${about ? html`<p class="about">${about}</p>` : ''}
+          <div class="icons" style="display: flex; align-items: center;">
+            ${github ? html`<a href="${githubLink}" target="_blank"><img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" width="18" height="18" /></a>` : ''}
+          <a style="text-decoration:none" href="${canonical}" target="_blank">📥</a>
+          </div>
         </div>
       </div>
     `
   }
 }
+
 
 class Contacts extends Component {
   render() {
@@ -326,6 +331,7 @@ export class App extends Component {
             picture="${me?.picture}"
             about="${me.about}"
             banner="${me.banner}"
+            github="${me.github}"
           />
           <${Contacts}
             contacts="${me.following}" userPublicKey="${key}"
